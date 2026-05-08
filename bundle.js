c2b8d28ab01e2d398575a5b3ef74f907665fa3ba
Onocle-33 <161675666+Onocle-33@users.noreply.github.com>
2026-05-09 01:02:28 +0900

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -30,8 +30,8 @@ var dmt = (function (exports, app$1, analytics) {
   snd3.preload="auto";
 
   window.onload= volume(),volume2();
-  document.getElementById("ent").addEventListener("click",entry());
-  document.getElementById("se").addEventListener("click",se());
+  document.getElementById("ent").onclick=entry();
+  document.getElementById("se").onclick=se();
   function entry(){
       let decks1=document.getElementById("decks").value;
       let names1=document.getElementById("names").value;
