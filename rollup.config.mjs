657a4c1c8664b1af3c55fef8a520ada00d39237c
Onocle-33 <161675666+Onocle-33@users.noreply.github.com>
2026-05-08 20:09:15 +0900

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,11 +2,12 @@
 
 export default {
   input: 'dmt.js',
-  external: ['vue', 'jquery'], // 外部モジュールとして扱う
+  external: ['vue', 'jquery', 'firebase/app','firebase/analytics','dmt'], // 外部モジュールとして扱う
   output: {
     file: 'bundle.js',
     format: 'iife',
     globals: {
+      './dmt.js':'dmt',
       'firebase/app': 'app$1',      // ← 左辺にパッケージ名、右辺に guessing 名
       'firebase/analytics': 'analytics',
       vue: 'Vue',         // import Vue from 'vue' -> window.Vue
