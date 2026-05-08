a7c6f50add3acd282519e734358ffd0db7b14f67
Onocle-33 <161675666+Onocle-33@users.noreply.github.com>
2026-05-08 20:12:01 +0900

    a

--- bundle.js
+++ bundle.js
@@ -1,4 +1,4 @@
-import {dmt} from './dmt.js'
+import dmt from './dmt.js'
 (function (app$1, analytics) {
   'use strict';
 
@@ -28,4 +28,4 @@ import {dmt} from './dmt.js'
   snd2.preload="auto";
   snd3.preload="auto";
 
-})/*(app$1, analytics)*/;
+});
