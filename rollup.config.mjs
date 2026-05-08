421997dcb159b62b18c4c224cc925a66e8692da5
Onocle-33 <161675666+Onocle-33@users.noreply.github.com>
2026-05-08 20:01:01 +0900

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -7,7 +7,8 @@ export default {
     file: 'bundle.js',
     format: 'iife',
     globals: {
-      dmtool:'app$1',
+      'firebase/app': 'app$1',      // ← 左辺にパッケージ名、右辺に guessing 名
+      'firebase/analytics': 'analytics',
       vue: 'Vue',         // import Vue from 'vue' -> window.Vue
       jquery: '$'        // import $ from 'jquery' -> window.$
     }
