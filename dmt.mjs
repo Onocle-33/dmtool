13fa59d252e9c3c343f386a3bb2c268e895db709
Onocle-33 <161675666+Onocle-33@users.noreply.github.com>
2026-05-08 13:16:02 +0900

    Update dmt.mjs

--- dmt.mjs
+++ dmt.mjs
@@ -1,5 +1,3 @@
-/*
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -21,7 +19,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-*/
+
 const ent=[];
 const chkEnt=[];
 var mode=0;
