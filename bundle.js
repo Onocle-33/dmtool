a258d5c29f9c9386a647ee05dcd1a0f8b696a784
Onocle-33 <161675666+Onocle-33@users.noreply.github.com>
2026-05-09 01:34:06 +0900

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -383,7 +383,7 @@ var dmt = (function (exports, app$1, analytics) {
       alert(res2 + "の先攻");
   }
   window.onload=function(){};
-  document.getElementById("ent").onclick=entry();
+  document.getElementById("ent").onclick=entry;
 
   exports.battle = battle;
   exports.chg = chg;
