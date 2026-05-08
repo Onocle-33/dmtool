bfe0c64390fba70f23dccc9840195f9a43044b61
Onocle-33 <161675666+Onocle-33@users.noreply.github.com>
2026-05-08 10:16:26 +0900

    Update dmt.js

--- dmt.js
+++ dmt.js
@@ -1,3 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDkYPx7NpFbdpmt-pNapu5FZn5HYWwCqvA",
+  authDomain: "dmtool-34a7d.firebaseapp.com",
+  projectId: "dmtool-34a7d",
+  storageBucket: "dmtool-34a7d.firebasestorage.app",
+  messagingSenderId: "682046414940",
+  appId: "1:682046414940:web:816aefbd845e13b02f8699",
+  measurementId: "G-YXQ3NGRF87"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const ent=[];
 const chkEnt=[];
 var mode=0;
@@ -10,6 +32,10 @@ snd.preload="auto"
 snd2.preload="auto"
 snd3.preload="auto"
 var count = 0;
+
+
+
+
 //setInterval(loading, 1000);
 function soundLoad(){
     snd.loop=false
