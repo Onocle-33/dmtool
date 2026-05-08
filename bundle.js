89c0b1665caf3bb697e8b777335398d410ba94b5
Onocle-33 <161675666+Onocle-33@users.noreply.github.com>
2026-05-08 17:10:27 +0900

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -1,5 +1,5 @@
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from './dmt.js';
+import { getAnalytics } from './dmt.js';
 
 // Import the functions you need from the SDKs you need
 // TODO: Add SDKs for Firebase products that you want to use
