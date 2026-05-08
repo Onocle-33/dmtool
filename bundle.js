b1062aca7012c427255df70c2acf2e85cb715563
Onocle-33 <161675666+Onocle-33@users.noreply.github.com>
2026-05-08 18:31:43 +0900

    a

--- bundle.js
+++ bundle.js
@@ -1,30 +1,30 @@
-'use strict';
+(function (app$1, analytics) {
+  'use strict';
 
-var app$1 = require('firebase/app');
-var analytics = require('firebase/analytics');
+  // Import the functions you need from the SDKs you need
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Import the functions you need from the SDKs you need
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDkYPx7NpFbdpmt-pNapu5FZn5HYWwCqvA",
+    authDomain: "dmtool-34a7d.firebaseapp.com",
+    projectId: "dmtool-34a7d",
+    storageBucket: "dmtool-34a7d.firebasestorage.app",
+    messagingSenderId: "682046414940",
+    appId: "1:682046414940:web:816aefbd845e13b02f8699",
+    measurementId: "G-YXQ3NGRF87"
+  };
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDkYPx7NpFbdpmt-pNapu5FZn5HYWwCqvA",
-  authDomain: "dmtool-34a7d.firebaseapp.com",
-  projectId: "dmtool-34a7d",
-  storageBucket: "dmtool-34a7d.firebasestorage.app",
-  messagingSenderId: "682046414940",
-  appId: "1:682046414940:web:816aefbd845e13b02f8699",
-  measurementId: "G-YXQ3NGRF87"
-};
+  // Initialize Firebase
+  const app = app$1.initializeApp(firebaseConfig);
+  analytics.getAnalytics(app);
+  const snd = new Audio("sound/剣で斬る6.mp3");
+  const snd2 = new Audio("sound/重力魔法1.mp3");
+  const snd3 = new Audio("sound/魔法陣を展開.mp3");
+  snd.preload="auto";
+  snd2.preload="auto";
+  snd3.preload="auto";
 
-// Initialize Firebase
-const app = app$1.initializeApp(firebaseConfig);
-analytics.getAnalytics(app);
-const snd = new Audio("sound/剣で斬る6.mp3");
-const snd2 = new Audio("sound/重力魔法1.mp3");
-const snd3 = new Audio("sound/魔法陣を展開.mp3");
-snd.preload="auto";
-snd2.preload="auto";
-snd3.preload="auto";
+})(app$1, analytics);
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,6 +4,10 @@ export default {
   input: 'dmt.js',
   output: {
     file: 'bundle.js',
-    format: 'cjs',
+    format: 'iife',
+    globals: {
+      vue: 'Vue',         // import Vue from 'vue' -> window.Vue
+      jquery: '$'        // import $ from 'jquery' -> window.$
+    }
   }
 };
